1cbd9f43883b34601c53d8584d75fca50726a7b4
Pallav-Choudhary <pallav.choudhary@42peaks.co>
2024-10-18 01:33:56 +0530

    PallavC0001 - Wrote the program for finding weather the number is even or odd

---
+++ src/main/java/com/example/javamavenjunithelloworld/Utility.java
@@ -0,0 +1,11 @@
+package com.example.javamavenjunithelloworld;
+
+public class Utility {
+    public static void main(String[] args) {
+
+    }
+
+    static boolean isEven(int n) {
+        return n%2==0;
+    }
+}
